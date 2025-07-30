01b5974ced7bf225e866fa2bd2a2879de9a63b2a
UneshNeupane <33648548+UneshNeupane@users.noreply.github.com>
2025-07-30 12:41:01 +1000

    Update index.js

--- src/components/demos/whs-advisor/index.js
+++ src/components/demos/whs-advisor/index.js
@@ -1,1 +1,64 @@
+import React from 'react';
+import { useWHSNavigation } from '../../../hooks/useWHSNavigation';
+import WHSAdvisorOverview from './WHSAdvisorOverview';
+import WHSActRegulationsScreen from './WHSActRegulationsScreen';
+import WHSCodesOfPracticeScreen from './WHSCodesOfPracticeScreen';
+import WHSIncidentResponseScreen from './WHSIncidentResponseScreen';
+import WHSCompensationScreen from './WHSCompensationScreen';
+import WHSComplaintsReportingScreen from './WHSComplaintsReportingScreen';
 
+const WHSAdvisorDemo = () => {
+  const navigation = useWHSNavigation();
+
+  const renderCurrentScreen = () => {
+    switch (navigation.currentScreen) {
+      case 'overview':
+        return <WHSAdvisorOverview onNext={navigation.navigateNext} />;
+      case 'act-regulations':
+        return (
+          <WHSActRegulationsScreen 
+            onNext={navigation.navigateNext}
+            onBack={navigation.navigatePrevious}
+          />
+        );
+      case 'codes-practice':
+        return (
+          <WHSCodesOfPracticeScreen 
+            onNext={navigation.navigateNext}
+            onBack={navigation.navigatePrevious}
+          />
+        );
+      case 'incident-response':
+        return (
+          <WHSIncidentResponseScreen 
+            onNext={navigation.navigateNext}
+            onBack={navigation.navigatePrevious}
+          />
+        );
+      case 'compensation':
+        return (
+          <WHSCompensationScreen 
+            onNext={navigation.navigateNext}
+            onBack={navigation.navigatePrevious}
+          />
+        );
+      case 'complaints-reporting':
+        return (
+          <WHSComplaintsReportingScreen 
+            onBack={navigation.navigatePrevious}
+            onReturnToStart={navigation.navigateToOverview}
+          />
+        );
+      default:
+        return <WHSAdvisorOverview onNext={navigation.navigateNext} />;
+    }
+  };
+
+  return (
+    <div className="whs-advisor-demo">
+      {renderCurrentScreen()}
+    </div>
+  );
+};
+
+export default WHSAdvisorDemo;
